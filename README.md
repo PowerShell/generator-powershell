147ba1a15d16b9b905f793d5a00aeca9e8c0c5f8
David Wilson <david@daviwil.com>
2015-11-09 14:39:05 -0800

    Add initial generator project structure
    This change introduces the basic structure for the generator-powershell
    project.  Actual generation behavior will be added in a later commit.

---
+++ README.md
@@ -0,0 +1,9 @@
+# PowerShell generator for Yeoman
+
+# Installing and Running
+
+```
+npm install -g generator-powershell
+
+yo powershell
+```
---
+++ generators/app/index.js
@@ -0,0 +1,5 @@
+var generators = require('yeoman-generator');
+
+module.exports = generators.Base.extend();
+
+// TODO: Follow instructions to continue: http://yeoman.io/authoring/
